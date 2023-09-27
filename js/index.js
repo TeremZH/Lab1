776a76ac3d99db1e9810046781210ba0255fd2e8
TeremZH <teremzhang@hotmail.com>
2023-09-27 17:09:46 -0600

    final js push

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
     var givenName = document.getElementById('givenname').value
     var lastName = document.getElementById('lastname').value
-
     console.log("First Name: " + givenName);
     console.log("Last Name: " + lastName);
+    var fullName = givenName + " " + lastName;
+    var outPut = document.getElementById("name");
+    outPut.textContent = "Full Name: " + fullName;
 })
 
 
