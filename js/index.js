9a3bf7ce49a44cc13162ea3652388e06613c6c06
TeremZH <teremzhang@hotmail.com>
2023-09-27 16:40:03 -0600

    FINAL PUSH

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('name')
+const form = document.getElementById('form')
+const givenname = document.getElementById('givenname')
+const lastname = document.getElementById('lastname') 
